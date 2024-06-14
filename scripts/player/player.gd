b611b39d41104ac473b7568c008ef87517b3a40f
micahhanevich <micahjhanevich@gmail.com>
2024-06-13 20:27:19 -0400

    finished adding hitboxes to room objects. Added pause menu

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody3D
 
 
-const SPEED = 9.0
-const JUMP_VELOCITY = 12.0
+const SPEED = 10.0
+const JUMP_VELOCITY = 30.0
 const MAX_LOOK_ANGLE = PI/2
+const STEP_HEIGHT = 10.0
+
+var Speed_Multiplier = 1.0
+
+# Keeps track of if the player has input movement since last frame
+var moving: bool = false
+
+var falling: bool = false
 
 # Define player properties
 @export_range(1, 10, 0.5)
@@ -20,22 +28,27 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta * 3
+		velocity.y -= gravity * delta * 10
 
 	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_pressed("move_sprint"):
+		Speed_Multiplier = 1.8
+	else:
+		Speed_Multiplier = 1.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * Speed_Multiplier
+		velocity.z = direction.z * SPEED * Speed_Multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * Speed_Multiplier)
+		velocity.z = move_toward(velocity.z, 0, SPEED * Speed_Multiplier)
 	
 	velocity = velocity.rotated(Vector3.UP, %PlayerCamPivot.rotation.y)
 	move_and_slide()
@@ -46,7 +59,10 @@ func _physics_process(delta):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	if Input.is_action_just_pressed("pause"):
-		get_tree().quit.call_deferred()
+		get_tree().paused = true
+		get_tree().root.add_child(preload("res://scenes/ui/pause_menu.tscn").instantiate())
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		# get_tree().quit.call_deferred()
 
 # Called when the mouse is moved
 func _input(event):
---
+++ scripts/ui/pause_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+var CanUnpause: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("pause") && CanUnpause:
+		close_menu()
+	else:
+		CanUnpause = true
+
+func close_menu():
+	queue_free()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+
+func close_game():
+	get_tree().quit()
