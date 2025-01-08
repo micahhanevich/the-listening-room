d75214762aff402e9fce4e163623b4aa4a25725b
micahhanevich <micahjhanevich@gmail.com>
2025-01-07 23:45:13 -0500

    pushicg shit out my ass

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -7,8 +7,7 @@ var CanUnpause: bool = false
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_just_pressed("pause") && CanUnpause:
 		close_menu()
 	else:
@@ -18,9 +17,11 @@ func close_menu():
 	set_visible(false)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
+	CanUnpause = false
+	$MapMenu.set_visible(false)
 
 func close_game():
 	get_tree().quit()
 
 func open_map_menu():
-	add_sibling(preload("res://scenes/ui/map_menu.tscn").instantiate())
+	$MapMenu.set_visible(true)
